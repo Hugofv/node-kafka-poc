2dac7bdf1ed0e96fb8bb163b67a131c398518d65
Hugo Fernandes <hugofv@hugos-mbp.lan>
2024-09-08 00:04:56 -0300

    Consume message..

--- kafka-consumer/src/index.js
+++ kafka-consumer/src/index.js
@@ -1,33 +1,32 @@
 const { Kafka } = require('kafkajs');
 
+// Create a new Kafka instance and configure the broker(s)
 const kafka = new Kafka({
   clientId: 'consumer-client',
-  brokers: ['kafka1:9092', 'kafka2:9092'],
+  brokers: ['localhost:9093'],
 });
 
-const consumer = kafka.consumer({ groupId: 'test-group' });
+// Create a consumer instance
+const consumer = kafka.consumer({ groupId: 'consumer-client' }); // Assign a group ID
 
-await consumer.connect();
-await consumer.subscribe({ topic: 'subscription', fromBeginning: true });
+const run = async () => {
+  // Connect to the consumer
+  await consumer.connect();
 
-await consumer.run({
-  /**
-   * This function is a callback provided to the Kafka consumer's `run` method.
-   * It is executed for each message consumed by the consumer.
-   *
-   * @param {Object} payload - The payload object containing information about the consumed message.
-   * @param {string} payload.topic - The topic from which the message was consumed.
-   * @param {number} payload.partition - The partition from which the message was consumed.
-   * @param {Object} payload.message - The Kafka message object.
-   * @param {Buffer} payload.message.value - The value of the message.
-   * @param {number} payload.message.offset - The offset of the message in the partition.
-   * @param {number} payload.message.timestamp - The timestamp of the message.
-   *
-   * @returns {void}
-   */
-  eachMessage: async ({ topic, partition, message }) => {
-    console.log({
-      value: message,
-    });
-  },
-});
+  // Subscribe to the topic
+  await consumer.subscribe({ topic: 'user-created', fromBeginning: true }); // Replace with your topic
+
+  // Run the consumer to read messages
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        partition,
+        offset: message.offset,
+        value: message.value.toString(), // The message value
+      });
+    },
+  });
+};
+
+// Handle any errors
+run().catch(console.error);
